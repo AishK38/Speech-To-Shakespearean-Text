82d6a902edcfa13c6bd3a592ed97f18ea08e078d
Aishwarya Karmakar <80018881+AishK38@users.noreply.github.com>
2021-08-11 18:43:33 +0530

    Completed

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 var speechRecognition = window.webkitSpeechRecognition;
 var recognition = new speechRecognition()
-var txtInput = $("#text-input")
 var content = ""
 var instructions = $("#instructions")
+var txtInput = $("#text-input")
 
+var btnTranslate = document.querySelector("#btn-translate")
+var output = document.querySelector("#output")
+var serverURL = "https://api.funtranslations.com/translate/shakespeare.json"
+
+btnTranslate.addEventListener("click", function clickHandler(){
+    fetch(getTranslatedtext(content))
+      .then(response => response.json())
+      .then(json => {
+          var translatedText = json.contents.translated
+          output.innerText = translatedText
+      })
+      .catch(errorHandler)
+})
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("Sorry, something wrong with server! Please try again after some time")
+}
+function getTranslatedtext(input) {
+    return serverURL + "?" + "text=" + input
+}
 recognition.continuous = true;
-
-recognition.onstart = function() {
-    instructions.text("Listening..")
-}
-
-recognition.onspeechend = function() {
-    instructions.text("Now click on the Translate button")
-}
 
 recognition.onresult = function(event) {
     var current = event.resultIndex;
@@ -31,3 +43,5 @@ $("#btn-start").click(function(event) {
 txtInput.on('input', function(){
     content = $(this).val()
 })
+
+
